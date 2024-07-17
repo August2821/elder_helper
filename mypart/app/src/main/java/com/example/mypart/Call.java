03ff8990f81b294e0f6976a13b8eb9843402400f
byeonsoyun <qusthdbs1@naver.com>
2024-07-17 18:57:03 +0900

    해치웠나?

---
+++ mypart/app/src/main/java/com/example/mypart/Call.java
@@ -0,0 +1,11 @@
+package com.example.mypart;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Call extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call); // activity_call.xml 레이아웃 파일 사용
+    }
+}
---
+++ mypart/app/src/main/java/com/example/mypart/Picture.java
@@ -0,0 +1,11 @@
+package com.example.mypart;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Picture extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture); // activity_call.xml 레이아웃 파일 사용
+    }
+}
